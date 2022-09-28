2e7ee94fe2b1df77b71f4b77c52704ab75d5f146
AndreyKonkin <parenekrus34@mail.ru>
2022-09-28 18:50:00 +0300

    apiRouter

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -15,4 +15,38 @@ apiRouter.route('/edit')
     await User.update(req.body, { where: {id: req}})
   })
 
+  apiRouter.route('/auth/registration')
+  .post(async (req, res) => {
+    const { name, password, email } = req.body;
+    if (name && password && email) {
+      try {
+        const user = await User.create({
+          ...req.body, password: await bcrypt.hash(password, 10),
+        });
+        const currUser = { id: user.id, name: user.name };
+        req.session.user = currUser;
+        return res.json(currUser);
+      } catch {
+        return res.sendStatus(500);
+      }
+    } else {
+      return res.sendStatus(401);
+    }
+  });
+
+  apiRouter.route('/auth/authorization')
+  .post(async (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+      const user = await User.findOne({ where: { email } });
+      if (user && await bcrypt.compare(password, user.password)) {
+        const currUser = { id: user.id, name: user.name };
+        req.session.user = currUser;
+        return res.json(currUser);
+      }
+      return res.sendStatus(401);
+    }
+    return res.sendStatus(401);
+  });
+
 export default apiRouter;
