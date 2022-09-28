a32ed04bf218588e905bd71b891b242249214a91
Vladimir <puzey@inbox.ru>
2022-09-28 17:56:57 +0300

    fix bugs with routers

--- db/models/user.js
+++ db/models/user.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'user_id',
       });
     }
-    }
+    
   }
   User.init({
     name: DataTypes.STRING,
--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { User } from '../db/models'
+import {User} from '../../db/models';
 
 const apiRouter = express.Router();
 
